5b3d809f62b53c2c18432dc9d5cdefa638aad151
Bjerkeset <112175751+Bjerkeset@users.noreply.github.com>
2022-12-07 15:05:46 +0100

    smaller edits

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("test")
 
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
+console.log("test")
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
